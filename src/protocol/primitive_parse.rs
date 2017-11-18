58ee498b38704e8bfdbb053f021b2cec9bb25e71
AustinHaugerud <austin15328@hotmail.com>
2017-11-18 14:05:28 -0800

    Added tests, modified functionality so that tests pass.

--- src/protocol/primitive_parse.rs
+++ src/protocol/primitive_parse.rs
@@ -1,23 +1,22 @@
 use std::str::*;
+use std::fmt::format;
 
-pub struct ReadBufferCursor<'a>
-{
+pub struct ReadBufferCursor<'a> {
   index: usize,
   bytes: &'a [u8],
 }
 
-impl<'a> ReadBufferCursor<'a>
-{
+impl<'a> ReadBufferCursor<'a> {
   pub fn new(buffer: &'a [u8]) -> ReadBufferCursor
   {
     ReadBufferCursor { index: 0, bytes: buffer }
   }
 
+  /// Parses an unsigned 16 bit integer from the buffer assuming little endianness.
   pub fn parse_u16l(&mut self) -> Result<u16, String>
   {
-    if self.bytes.len() - (self.index + 1) < 2
-    {
-      return Err(String::from("Not enough bytes remaining to parse u16."))
+    if self.bytes.len() - self.index + 1 < 2 {
+      return Err(String::from("Not enough bytes remaining to parse u16."));
     }
 
     let result = parse_u16l(&self.bytes[self.index..(self.index + 2)]);
@@ -25,11 +24,11 @@ impl<'a> ReadBufferCursor<'a>
     Ok(result)
   }
 
+  /// Parses a signed 16 bit integer the buffer assuming little endianness.
   pub fn parse_i16l(&mut self) -> Result<i16, String>
   {
-    if self.bytes.len() - (self.index + 1) < 2
-    {
-      return Err(String::from("Not enough bytes remaining to parse i16."))
+    if self.bytes.len() - self.index + 1 < 2 {
+      return Err(String::from("Not enough bytes remaining to parse i16."));
     }
 
     let result = parse_i16l(&self.bytes[self.index..(self.index + 2)]);
@@ -37,32 +36,26 @@ impl<'a> ReadBufferCursor<'a>
     Ok(result)
   }
 
-  pub fn parse_var_string(&mut self) -> Result<&str, String>
-  {
-    let size_result = self.parse_u16l();
-
-    if size_result.is_err()
-    {
-      return Err(String::from("Failed to parse string size."))
-    }
-
-    let size = size_result.unwrap() as usize;
-
-    let result = parse_var_string(&self.bytes[self.index..(self.index + size)]);
-    self.index += size;
-
-    match result
-    {
-      Ok(T) => Ok(T),
-      Err(E) => Err(String::from("Failed to parse string."))
+  /// Parses a variable sized string from the buffer by reading a u16 length
+  /// descriptor(little endian) then the string itself.
+  pub fn parse_var_string(&mut self) -> Result<String, String> {
+    match parse_var_string(&self.bytes) {
+      Ok(T) => {
+        self.index += T.len() + 2;
+        Ok(T)
+      },
+      Err(E) => Err(E)
     }
   }
 
-  pub fn parse_string(&mut self, capacity: u16) -> Result<&str, Utf8Error>
-  {
+  /// Parses a fixed size string up to its capacity or null terminator.
+  pub fn parse_string(&mut self, capacity: u16) -> Result<String, String> {
     let result = parse_string(&self.bytes[self.index..(self.index + (capacity as usize))], capacity);
     self.index += capacity as usize;
-    result
+    match result {
+      Ok(T) => Ok(T.to_string()),
+      Err(_) => Err(String::from("Failed to parse string."))
+    }
   }
 }
 
@@ -71,31 +64,122 @@ fn parse_u16l(bytes: &[u8]) -> u16
   (bytes[0] as u16 | ((bytes[1] as u16) << 8))
 }
 
-fn parse_i16l(bytes: &[u8]) -> i16
-{
+fn parse_i16l(bytes: &[u8]) -> i16 {
   let val = parse_u16l(bytes);
 
   if val < 0x7fff { val as i16 } else { -1 - (0xffffu32 - (val as u32)) as i16 }
 }
 
-fn parse_var_string(bytes: &[u8]) -> Result<&str, Utf8Error>
+fn parse_var_string(bytes: &[u8]) -> Result<String, String>
 {
-  from_utf8(&bytes)
+  if bytes.len() < 2 {
+    return Err(String::from("Not enough bytes to read a string."))
+  }
+
+  let size = parse_u16l(&bytes) as usize;
+
+  if size > bytes.len() - 2 {
+    return Err(String::from(format!("Length descriptor {} asks for more bytes than present.", size)))
+  }
+
+  match from_utf8(&bytes[2..(2 + size)]) {
+    Ok(T) => Ok(T.to_string()),
+    Err(_) => Err(String::from("Failed to parse utf8 data."))
+  }
 }
 
-fn parse_string(bytes: &[u8], capacity: u16) -> Result<&str, Utf8Error>
-{
-  let mut index: usize = 0;
+fn parse_string(bytes: &[u8], capacity: u16) -> Result<String, String> {
+  let mut index: usize = capacity as usize;
 
-  for i in 0..capacity
-    {
-      if bytes[i as usize] == 0
-        {
-          index = i as usize;
-          break;
-        }
+  for i in 0..capacity {
+    if bytes[i as usize] == 0 {
+      index = i as usize;
+      break;
     }
+  }
 
-  return from_utf8(&bytes[0..index]);
+  match from_utf8(&bytes[0..index]) {
+    Ok(T) => Ok(T.to_string()),
+    Err(_) => Err(String::from("Failed to parse utf8 data."))
+  }
 }
 
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  #[test]
+  fn test_cursor_u16l_read() {
+    let data = [0xf8u8, 0xffu8];
+    let mut cursor = ReadBufferCursor::new(&data);
+
+    assert_eq!(cursor.parse_u16l().unwrap(), 0xff_f8_u16);
+  }
+
+  #[test]
+  fn test_cursor_i16l_read() {
+    let data = [0xff, 0x00];
+    let mut cursor = ReadBufferCursor::new(&data);
+
+    assert_eq!(cursor.parse_i16l().unwrap(), 0x00_ff_i16);
+  }
+
+  #[test]
+  fn test_cursor_var_string_read() {
+    let data = [0x03, 0x00, 'c' as u8, 'a' as u8, 't' as u8];
+    let mut cursor = ReadBufferCursor::new(&data);
+
+    assert_eq!(cursor.parse_var_string().unwrap(), String::from("cat"));
+  }
+
+  #[test]
+  fn test_cursor_string_read_null_terminated() {
+    let data = ['c' as u8, 'a' as u8, 't' as u8, '\0' as u8];
+    let mut cursor = ReadBufferCursor::new(&data);
+
+    assert_eq!(cursor.parse_string(4).unwrap(), String::from("cat"));
+  }
+
+  #[test]
+  fn test_cursor_string_read_full() {
+    let data = ['c' as u8, 'a' as u8, 't' as u8];
+    let mut cursor = ReadBufferCursor::new(&data);
+
+    assert_eq!(cursor.parse_string(3).unwrap(), String::from("cat"));
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
+  #[test]
+  fn test_u16l_read() {
+    let data = [0xf8, 0xff];
+    assert_eq!(parse_u16l(&data), 0xff_f8_u16);
+  }
+
+  #[test]
+  fn test_i16l_read() {
+    let data = [0xff, 0x00];
+    assert_eq!(parse_i16l(&data), 0x00_ff_i16);
+  }
+
+  #[test]
+  fn test_var_string_read() {
+    let data = [0x03, 0x00, 'c' as u8, 'a' as u8, 't' as u8];
+    assert_eq!(parse_var_string(&data).unwrap(), String::from("cat"));
+  }
+
+  #[test]
+  fn test_string_read_null_terminated() {
+    let data = ['c' as u8, 'a' as u8, 't' as u8, '\0' as u8];
+    assert_eq!(parse_string(&data, 4).unwrap(), String::from("cat"));
+  }
+
+  #[test]
+  fn test_string_read_full() {
+    let data = ['c' as u8, 'a' as u8, 't' as u8];
+    assert_eq!(parse_string(&data, 3).unwrap(), String::from("cat"));
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+}
