705ab9d0522fc7eb74d2ddf5a1a04e52b6d8b7cc
AustinHaugerud <austin15328@hotmail.com>
2018-04-05 21:44:02 -0700

    Change to allow writing for lurk message references

--- src/protocol/send.rs
+++ src/protocol/send.rs
@@ -12,6 +12,10 @@ impl<'a, T> LurkSendChannel<'a, T> where T: Write {
   }
 
   pub fn write_message<F>(&mut self, message : F) -> Result<(), ()> where F: LurkMessageBlobify {
+    self.write_message_ref(&message)
+  }
+
+  pub fn write_message_ref<F>(&mut self, message: &F) -> Result<(), ()> where F: LurkMessageBlobify {
     let mut data = message.produce_lurk_message_blob();
 
     if self.target.write_all(&mut data).is_err() {
